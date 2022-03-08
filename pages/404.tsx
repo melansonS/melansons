20510f3759139b47e826b3142c9541bad2ef8479
Samuel Melanson <samuelmelanson@live.com>
2022-03-08 15:08:49 -0500

    404 page

---
+++ pages/404.tsx
@@ -0,0 +1,9 @@
+import React, { useEffect } from "react";
+import { useRouter } from "next/router";
+export default function Custom404() {
+  const router = useRouter();
+  useEffect(() => {
+    router.push("/");
+  }, []);
+  return <div />;
+}
